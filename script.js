5d2975437459d9c058bc1c56011b51ec39d4143c
Akash0113 <makash2813@gmail.com>
2025-09-21 13:41:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file is currently empty because we added `scroll-behavior: smooth;` to the CSS.
+// This is a more modern and efficient way to handle smooth scrolling.
+// You can keep this file here for any future interactivity you want to add!
+
+console.log("Portfolio loaded successfully!");
